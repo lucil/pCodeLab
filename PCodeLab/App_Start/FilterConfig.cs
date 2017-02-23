79cb0bfb1bae66012bc9af1c77b7c036464726a5
Emil Trebicka <etrebicka@remedia.it>
2017-02-23 15:19:25 +0100

    First commit

---
+++ PCodeLab/App_Start/FilterConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace PCodeLab
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ PCodeLab/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace PCodeLab.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.CurrentYear = DateTime.Now.Year;
+            return View();
+        }
+
+        private static string BuildMailBody(string name, string email, string message)
+        {
+            var res = "<p>You have a new contact from PCodelab: </p>";
+            res += "<strong>Name: </strong>" + name + "<br/>";
+            res += "<strong>Email: </strong>" + email + "<br/>";
+            res += "<strong>Message: </strong>" +  message + "<br/>";
+            return res;
+        }
+    }
+}
